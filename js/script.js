a94e082697b590388ddd6c138a5e12f02ef6d297
Ravshanbek <37891149+aximas@users.noreply.github.com>
2021-02-25 19:10:13 +0500

    pre last changed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    var maxHeight = document.querySelector('body').scrollHeight;
+    // const welcomeBlockHeight = document.querySelector('section.welcome').scrollHeight;
+    const welcomeBlock = document.querySelector('section.welcome'),
+          body = document.querySelector('body'),
+          sectionExperience = document.querySelector('section.experience'),
+          centerLine = sectionExperience.querySelector('.central-line');
 
-    console.log(maxHeight);
+          window.addEventListener('scroll', () => {
 
-    // const leftLine = document.querySelector('.left-line'),
-    //       rightLine = document.querySelector('.right-line');
+                if (window.pageYOffset > welcomeBlock.clientHeight + +((sectionExperience.clientHeight)*0.1) 
+                    && 
+                    window.pageYOffset < (+(welcomeBlock.clientHeight) + +(sectionExperience.clientHeight))) {
+                    // centerLine.top += 
+                    var counter = ((window.pageYOffset - 900)/6);
+                    var counterMinus = +(90 - +((window.pageYOffset - 900)/6));
+                    console.log('counterPlus', counter);
+                    console.log('counterMinus', counterMinus);
 
-    // leftLine.style.setProperty('--element-height', `${maxHeight}` + 'px');
-    // rightLine.style.setProperty('--element-height', `${maxHeight}` + 'px');
+                    centerLine.style.top =+ counter + '%';
+                    centerLine.style.height =+ counterMinus + '%';
+                }
+
+                else {
+                    centerLine.style.top = 10 + '%';
+                    centerLine.style.height = 80 + '%';
+                }
+          });
+
+    console.log(welcomeBlock);
     
 
     // function disablecontext(e) {
@@ -21,12 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // var errorMsg = "Вы не можете сохранять изображения с этого сайта.";
     // document.oncontextmenu = disablecontext;
 
-    document.querySelectorAll('input').forEach((item) => {
-        var inputsValue = []
+  
 
-        inputsValue.push(item.value);
 
-    })
 
     
 })
